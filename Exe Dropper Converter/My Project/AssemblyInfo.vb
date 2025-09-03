25c21b44a9738dd26b874c74f68e5a83b0c2fe38
twelve2072 <asdhauiewh28934@outlook.com>
2025-09-03 12:32:31 -0700

    AssemblyInfo.vb

---
+++ Exe Dropper Converter/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+' связанных со сборкой.
+<Assembly: AssemblyTitle("ExeConverter")>
+<Assembly: AssemblyDescription("Convert your exe files to bat, vbs, js and ps1")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("KKosty4ka")>
+<Assembly: AssemblyProduct("ExeConverter")>
+<Assembly: AssemblyCopyright("Copyright ©KKosty4ka 2022")>
+<Assembly: AssemblyTrademark("ExeConverter")>
+<Assembly: AssemblyCulture("")>
+
+' Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+' для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+' COM, следует установить атрибут ComVisible в TRUE для этого типа.
+<Assembly: ComVisible(False)>
+
+' Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("a8000199-9f91-4751-8a05-396b2afca4c7")>
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'      Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' [assembly: AssemblyVersion("1.0.*")]
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
